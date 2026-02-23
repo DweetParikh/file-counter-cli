ecc13632aa16c90c5b3f094d5504931797f618cf
Dweet Parikh <dweet.parikh16@gmail.com>
2026-02-24 00:51:55 +0530

    added commands

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { Commander, program, Command } = require('commander')
-const fs = require('fs')
+const fs = require('fs');
+const { console } = require('inspector');
 const program = new Command();
 
 program
@@ -11,5 +12,24 @@ program.command('words')
     .description('Counts number of words in a text file')
     .argument('<file>', 'words to count in a text file')
     .action((file) => {
-        fs.readFile(file, 'utf-8', (err, data))
-    })
+        fs.readFile(file, 'utf-8', (err, data) => {
+            if(err) {
+                console.log(err);
+            } else {
+                const words = data.split(" ").length;
+                console.log(`There are ${words} words in ${file}.`);
+            }
+        });
+    });
+
+program.command('lines')
+    .description('Counts number of lines in a text file')
+    .argument('<file>', 'lines to count in a text file')
+    .action((file) => {
+        fs.readFile(file, 'utf-8', (err, data) => {
+            const lines = data.split("\n").length;
+            console.log(`There are ${lines} lines in ${file}.`)
+        });
+    });
+
+program.parse();
