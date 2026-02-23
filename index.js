9eb325c692d36a31b54ca9c82e46c4135c087059
Dweet Parikh <dweet.parikh16@gmail.com>
2026-02-24 00:24:11 +0530

    added comands

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
- 
+const { Commander, program, Command } = require('commander')
+const fs = require('fs')
+const program = new Command();
+
+program
+    .name('File related CLI')
+    .description('CLI for file related task')
+    .version('0.1.0')
+
+program.command('words')
+    .description('Counts number of words in a text file')
+    .argument('<file>', 'words to count in a text file')
+    .action((file) => {
+        fs.readFile(file, 'utf-8', (err, data))
+    })
